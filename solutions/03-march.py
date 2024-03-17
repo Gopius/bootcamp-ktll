f24d172df9cae399f834f5192a81e7c989040586
Godwin Pius Ekpo <godwinpius.gp@gmail.com>
2024-03-17 19:44:47 +0100

    my first assignment

---
+++ solutions/03-march.py
@@ -0,0 +1,19 @@
+statement = 'Python is fun!'
+print(statement[1:5])
+print(statement[0:5])
+print(statement[1:14:1])
+print(statement[0:14])
+print(statement[1:12:2])
+print(statement[1:12:3])
+print(statement[0:14:2])
+
+
+"Question 2"
+x = 'hello'
+y = '(,)'
+z = 'world'
+xy = x+z
+print(xy)
+print(xy[1:5])
+print(xy[1:9])
+print(xy[1:8:2])
